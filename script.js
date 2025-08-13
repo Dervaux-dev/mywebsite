166e4fb9a1c3b0b01302e252ea7967d26cd8ad0f
Dervaux_dev <inezadelvo@gmail.com>
2025-08-13 17:49:25 +0200

    my

---
+++ script.js
@@ -0,0 +1,9 @@
+// Highlight active link on click
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function() {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
